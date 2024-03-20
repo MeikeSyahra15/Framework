a43a81f6d144c998c4ce167bda6fc1499297fb37
MeikeSyahra15 <164151958+MeikeSyahra15@users.noreply.github.com>
2024-03-20 15:26:59 +0700

    Add files via upload

---
+++ storage/framework/views/f567cfa1812a0bb8a19635f8f287189d.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>personal web meike</title>
+    <link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.
+css">
+
+    <link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css
+">
+
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="images/taurus.png" alt="" width="50" height="35">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Portofolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    
+    <div class="bg-light mt-5" id="main">
+      <!-- Container -->
+      <div class="container py-5 px-4">
+          <div class="row">
+              <div class="col-md-5 order-md-2">
+                  <img class="img-fluid" src="images/foto meike.png" alt="main logo">
+              </div>
+              <div class="col-md-7 order-md-1">
+                  <h1 class="mt-4 display-3"><b> Hello I'm Meike!!</b></h1>
+                  <p class="fs-5 mt-3">I am someone who likes to learn new things and is 
+                    stubborn of course because I am the last child. Has a story-telling nature but is introverted. 
+                    And that's me.I am a taurus known as a very tough and persistent individual. They don't like rapid changes and prefer to have a structured routine. 
+                    They are deeply attached to traditional values ​​and have a strong drive to create stability in their lives, be it in terms of finances, relationships, or careers.</p>
+                  <div class="row">
+                      <div class="d-flex flex-column flex-md-row">
+                        <button type="button" class="btn btn-secondary">Introduction</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <div id="footer" class="bg-light py-5">
+    <!-- Container -->
+    <div class="container py-5 px-4">
+        <div class="row">
+            <div class="col-lg-3 mb-5">
+                <a href="#" class="logo text-decoration-none">
+                    <div class="d-flex">
+                        <img class="img-fluid" src="images/taurus.png" alt="Bootstrap Logo" style="width: 40px;">
+                        <div class="fs-5 ms-2 text-black">Taurus</div>
+                    </div>
+                </a>
+                <div class="mt-2 text-muted">
+                    <small>a zodiac runs from April 20 to May 20. People born between these dates are considered to have the zodiac sign Taurus.<a href="#" class="text-black">Taurus Team</a> with the help of 
+                    <a href="#" class="text-black">our contributors</a>.</small>
+                </div>
+                <div class="mt-2 text-muted">
+                    <small>Code licensed <a href="#" class="text-black">MIT</a>, docs <a href="#" class="text-black">CC BY 3.0</a>.</small>
+                </div>
+                <div class="mt-2 text-muted">
+                    <small>Currently v5.3.0-alpha2.</small>
+                </div>
+            </div>
+            <div class="col-6 col-lg-2 offset-lg-1 mb-5">
+            <h5>Links</h5>
+            <ul class="list-unstyled">
+                <li class="mb-2"><a href="#">Home</a></li>
+                <li class="mb-2"><a href="#">About</a></li>
+                <li class="mb-2"><a href="#">Portofolio</a></li>
+                <li class="mb-2"><a href="#">Blog</a></li>
+                <li class="mb-2"><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-lg-2 mb-5">
+            <h5>Guides</h5>
+            <ul class="list-unstyled">
+                <li class="mb-2"><a href="#">Getting started</a></li>
+                <li class="mb-2"><a href="#">Starter template</a></li>
+                <li class="mb-2"><a href="#">Webpack</a></li>
+                <li class="mb-2"><a href="#">Parcel</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-lg-2 mb-5">
+            <h5>Projects</h5>
+            <ul class="list-unstyled">
+                <li class="mb-2"><a href="#">Bootstrap 5</a></li>
+                <li class="mb-2"><a href="#">Bootstrap 4</a></li>
+                <li class="mb-2"><a href="#">Icons</a></li>
+                <li class="mb-2"><a href="#">RFS</a></li>
+                <li class="mb-2"><a href="#">npm starter</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-lg-2 mb-5">
+            <h5>Community</h5>
+            <ul class="list-unstyled">
+                <li class="mb-2"><a href="#">Issues</a></li>
+                <li class="mb-2"><a href="#">Discussions</a></li>
+                <li class="mb-2"><a href="#">Corporate sponsors</a></li>
+                <li class="mb-2"><a href="#">Open Collective</a></li>
+                <li class="mb-2"><a href="#">Slack</a></li>
+                <li class="mb-2"><a href="#">Stack overflow</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
+<script
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle
+.min.js"></script>
+</body>
+</html<?php /**PATH C:\xampp\htdocs\basic-router\resources\views/meike.blade.php ENDPATH**/ ?>
